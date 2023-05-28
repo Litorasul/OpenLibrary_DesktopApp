2119b2387721cb0847db8ed63e3bff9c33354c05
Nikolay Kelov <n_kelov@yahoo.com>
2023-05-28 11:56:42 +0200

    Add GlobalVariables, add ApiClientHandler, initialize HttpClient

---
+++ src/OpenLibrary/OpenLibrary_DataAccess/Infrastructure/ApiClientHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+using static OpenLibrary_DataAccess.Infrastructure.GlobalVariables;
+
+namespace OpenLibrary_DataAccess.Infrastructure;
+
+public static class ApiClientHandler
+{
+    public static HttpClient? ApiClient { get; set; }
+
+    public static void InitializeClient()
+    {
+        ApiClient = new HttpClient();
+        ApiClient.BaseAddress = new Uri(OPEN_LIBRARY_BASE_URL);
+        ApiClient.DefaultRequestHeaders.Accept.Clear();
+        ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+}
---
+++ src/OpenLibrary/OpenLibrary_DataAccess/Infrastructure/GlobalVariables.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenLibrary_DataAccess.Infrastructure;
+
+public static class GlobalVariables
+{
+    #region Open Library URL parts
+
+    public const string OPEN_LIBRARY_BASE_URL = "https://openlibrary.org/search.json?";
+    public const string OPEN_LIBRARY_FULL_SEARCH = "q=";
+    public const string OPEN_LIBRARY_TITLE_SEARCH = "title=";
+    public const string OPEN_LIBRARY_AUTHOR_SEARCH = "author=";
+
+    #endregion
+}
